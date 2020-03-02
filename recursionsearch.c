f19fb5ce1422764f096e181cbf8ca6c988f6c21e
P Sai Deekshith <56923148+deekshith39@users.noreply.github.com>
2020-03-02 22:41:43 +0530

    Update recursionsearch.c

--- recursionsearch.c
+++ recursionsearch.c
@@ -1,69 +1,73 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<conio.h>
 #include<time.h>
-int a[10],i,n,key;
-int mid,beg,end,pos;
-int bs(int key,int beg,int end,int a[]);
-int startt,timeln,endt,timebn;
-
-
-int ls(int key,int i)
-{
-
-if(a[i]==key)
-{
-printf("element found at positon %d",i);
-
-}
-else
-{
-
-return ls(key,i++);
-}
-
-}
-
+int ls(int key,int i);
+int bs(int key,int beg,int end1,int a[]);
+int key,i,a[10],n,result,choice;
+int beg,end1,mid;
+clock_t start,end;
+double time1;
 void main()
 {
-int q;
-printf("enter the size of the array = ");
-scanf("%d",&n);
-printf("enter the elements into the array = ");
-for(int i=0;i<n;i++)
-scanf("%d",&a[i]);
-printf("enter th element to be searched = ");
-scanf("%d",&key);
-printf("choice= ");
-scanf("%d",&q);
-if(q==0){
-startt=clock();
-ls(key,0);
-endt=clock();
-timeln=endt-startt;}
-else{
-startt=clock();
-bs(key,0,n-1,a);
-endt=clock();
-timebs=endt-startt;
-}
-}
+    printf("enter the no. of elements in the array");
+    scanf("%d",&n);
+    printf("enter the elements into the array");
+    for(int i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    printf("enter the element to be searched");
+    scanf("%d",&key);
+    i=0;
+    printf("enter the choice:");
+    scanf("%d",&choice);
+    if(choice==1)
+    {
+    start=clock();
+    result=ls(key,i);
+    end=clock();
+    time1=(end-start)/CLOCKS_PER_SEC;
+    }
+    else
+    {
+        beg=0;
+        end1=n-1;
 
-int bs(int key,int beg,int end,int a[])
-{
-mid=(beg+end)/2;
-if(key==a[mid]){
-printf("element found at position %d",mid);
-exit(0);
-}
-else if(a[mid]>key)
-{
-end=mid-1;
-return bs(key,beg,end,a);
-}
-else
-{
-beg=mid+1;
-return bs(key,beg,end,a);
+        start=clock();
+    result=bs(key,beg,end1,a);
+        end=clock();
+    }
+    printf("result =%f",time1);
+    if(result==-1)
+        printf("not found");
+    else
+        printf("element is found at position %d",result);
+
 
 }
+
+ls(int key,int i)
+{
+    if(i>n)
+        return -1;
+    if(key==a[i])
+        return i;
+    else
+        return ls(key,++i);
+
+}
+
+bs(int key,int beg,int end1,int a[])
+{
+    mid=(beg+end1)/2;
+    if(beg<end1)
+    {
+    if(a[mid]==key)
+        return mid;
+    else if(a[mid]>key)
+        return bs(key,beg,mid-1,a);
+    else
+        return bs(key,mid+1,end1,a);
+
+    }
+    else return -1;
+
 }
